921c1142bef556a68d1cd75dc81508785d53e39f
Bard Liao <yung-chuan.liao@linux.intel.com>
2023-04-28 10:42:56 +0800

    topology2: add sof-tgl-amp.tplg support
    For test only. rt1316 + rt1318
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/avs-tplg/tplg-targets.cmake
+++ tools/topology/topology2/avs-tplg/tplg-targets.cmake
@@ -24,6 +24,11 @@ PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-tgl-rt711-rt1316-rt714.bin"
 
 "cavs-sdw\;sof-adl-rt711-l0-rt1316-l12-rt714-l3\;NUM_SDW_AMP_LINKS=2,SDW_DMIC=1"
 
+"cavs-sdw\;sof-tgl-amp\;NUM_SDW_AMP_LINKS=2,SDW_DMIC=0,SDW_JACK=false,\
+SDW_SPK_STREAM=SDW0-Playback,SDW_SPK_IN_STREAM=SDW0-Capture,DEEPBUFFER_FW_DMA_MS=false,\
+HDMI1_ID=4,HDMI2_ID=5,HDMI3_ID=6"
+
+
 # IPC4 topology for TGL rt711 Headset + rt1308 Amplifier + rt715 DMIC
 "cavs-sdw\;sof-tgl-rt715-rt711-rt1308-mono\;NUM_SDW_AMP_LINKS=1,SDW_DMIC=1,\
 SDW_JACK_OUT_STREAM=SDW1-Playback,SDW_JACK_IN_STREAM=SDW1-Capture,\
