13f83845cff41ef0ade32160e7dd1da6559c6f0c
Bard Liao <yung-chuan.liao@linux.intel.com>
2023-10-11 09:04:05 +0800

    topology2: add sof-mtl-cs42l43-l0-cs35l56-l12.tplg
    Add cs42l43 + cs35l56 topology for MTL.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
+++ tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
@@ -39,6 +39,11 @@ SDW_DMIC_STREAM=SDW0-Capture"
 SDW_SPK_STREAM=Playback-SmartAmp,SDW_SPK_IN_STREAM=Capture-SmartAmp,\
 SDW_DMIC_STREAM=Capture-SmartMic,SDW_JACK_OUT_STREAM=Playback-SimpleJack,\
 SDW_JACK_IN_STREAM=Capture-SimpleJack"
+
+"cavs-sdw\;sof-mtl-cs42l43-l0-cs35l56-l12\;PLATFORM=mtl,NUM_SDW_AMP_LINKS=2,SDW_DMIC=1,\
+NUM_HDMIS=0,SDW_SPK_STREAM=SDW1-Playback,SDW_SPK_IN_STREAM=SDW1-Capture,SDW_DMIC_STREAM=Capture-SmartMic,\
+SDW_JACK_IN_STREAM=Capture-SimpleJack,SDW_AMP_FMT_24=true,SDW_JACK_FMT_24=true"
+
 # Below topologies are used on Chromebooks
 
 "cavs-rt5682\;sof-mtl-max98357a-rt5682\;PLATFORM=mtl,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,\
